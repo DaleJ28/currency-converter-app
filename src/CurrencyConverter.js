c0a1e4b6ac839df2f5ae454f6060bb9a8d51d3c8
Dale Jiblal <70293940+DaleJ28@users.noreply.github.com>
2023-09-26 09:10:53 -0400

    + fetch request comp.didmount, + acr. update func.

--- src/CurrencyConverter.js
+++ src/CurrencyConverter.js
@@ -1,93 +1,130 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.css';
+import currencies from './utils/currencies';
+import { checkStatus } from './utils/fetchUtils';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 
 class CurrencyConverter extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        rate: 0.89,
-        usd: 1,
-        euro: 1 * 0.89,
-      };
-  
-      this.handleUsdChange = this.handleUsdChange.bind(this);
-      this.handleEuroChange = this.handleEuroChange.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      rate: 109.55,
+      baseAcronym: 'USD',
+      baseValue: 1,
+      quoteAcronym: 'JPY',
+      quoteValue: 1 * 109.55,
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    const { baseAcronym, quoteAcronym } = this.state;
+    this.getRate(baseAcronym, quoteAcronym);
+  }
+
+  getRate = (base, quote) => {
+    this.setState({ loading: true });
+    fetch(`https://api.frankfurter.app/latest?from=${base}&to=${quote}`)
+      .then(checkStatus)
+      .then(json)
+      .then(data => {
+        if (data.error) {
+          throw new Error(data.error);
+        }
+        const rate = data.rates[quote];
+        this.setState({
+          rate,
+          baseValue: 1,
+          quoteValue: Number((1 * rate).toFixed(3)),
+          loading: false,
+        });
+      })
+      .catch(error => console.error(error.message));
+  }
+
+  toBase(amount, rate) {
+    return amount * (1 / rate);
+  }
+  toQuote(amount, rate) {
+    return amount * rate;
+  }
+  convert(amount, rate, equation) {
+    const input = parseFloat(amount);
+    if (Number.isNaN(input)) {
+      return '';
     }
-  
-  
-    toUsd(amount, rate) {
-      return amount * (1 / rate);
+    return equation(input, rate).toFixed(3);
+  }
+  changeBaseAcronym = (event) => {   //handle changing the currency selection in the state.
+    const baseAcronym = event.target.value;
+    this.setState({ baseAcronym });
+  }
+  changeBaseValue = (event) => {
+    const quoteValue = this.convert(event.target.value, this.state.rate, this.toQuote);
+    this.setState({
+      baseValue: event.target.value,
+      quoteValue,
+    });
+  }
+  changeQuoteAcronym = (event) => {   //handle changing the currency selection in the state.
+    const quoteAcronym = event.target.value;
+    this.setState({ quoteAcronym });
+    this.getRate(this.state.baseAcronym, quoteAcronym);
     }
-  
-    toEuro(amount, rate) {
-      return amount * rate;
-    }
-  
-    convert(amount, rate, equation) {
-      const input = parseFloat(amount);
-      if (Number.isNaN(input)) {
-        return '';
-      }
-      return equation(input, rate).toFixed(3);
-    }
-  
-    handleUsdChange(event) {
-      const euro = this.convert(event.target.value, this.state.rate, this.toEuro);
-      this.setState({
-        usd: event.target.value,
-        euro
-      });
-    }
-  
-    handleEuroChange(event) {
-      const usd = this.convert(event.target.value, this.state.rate, this.toUsd);
-      this.setState({
-        euro: event.target.value,
-        usd
-      });
-    }
-  
-  
-  
-    render() {
-      const { rate, usd, euro } = this.state;
-  
-      return (
-        <div className="container">
-          <div className="text-center p-3 mb-2">
-            <h2 className="mb-2">Currency Converter</h2>
-            <h4>USD 1 : {rate} EURO</h4>
-          </div>
-          <div className="row text-center">
-            <div className="col-12">
-              <span className="mr-1">USD</span>
-              <CurrencyInput value={usd} onChange={this.handleUsdChange} type="number" />
-              <span className="mx-3">=</span>
-              <CurrencyInput value={euro} onChange={this.handleEuroChange} type="number" />
-              <span className="ml-1">EURO</span>
-            </div>
-          </div>
+    
+  changeQuoteValue = (event) => {
+    const baseValue = this.convert(event.target.value, this.state.rate, this.toBase);
+    this.setState({
+      quoteValue: event.target.value,
+      baseValue,
+    });
+  }
+  render() {
+    const { rate, baseAcronym, baseValue, quoteAcronym, quoteValue, loading } = this.state;
+    const currencyOptions = Object.keys(currencies).map(currencyAcronym => <option key={currencyAcronym} value={currencyAcronym}>{currencyAcronym}</option>);
+    return (
+      <React.Fragment>
+        <div className="text-center p-3">
+          <h2 className="mb-2">Currency Converter</h2>
+          <h4>1 {baseAcronym} to 1 {quoteAcronym} = {rate} {currencies[quoteAcronym].name}</h4>
         </div>
-      )
-    }
+        <form className="form-row p-3 bg-light justify-content-center">
+          <div className="form-group col-md-5 mb-0">
+            <select value={baseAcronym} onChange={this.changeBaseAcronym} className="form-control form-control-lg mb-2" disabled={loading}>
+              {currencyOptions}
+            </select>
+            <div className="input-group">
+              <div className="input-group-prepend">
+                <div className="input-group-text">{currencies[baseAcronym].symbol}</div>
+              </div>
+              <input id="base" className="form-control form-control-lg" value={baseValue} onChange={this.changeBaseValue} type="number" />
+            </div>
+            <small className="text-secondary">{currencies[baseAcronym].name}</small>
+          </div>
+          <div className="col-md-2 py-3 d-flex justify-content-center align-items-center">
+            <h3>=</h3>
+          </div>
+          <div className="form-group col-md-5 mb-0">
+            <select value={quoteAcronym} onChange={this.changeQuoteAcronym} className="form-control form-control-lg mb-2" disabled={loading}>
+              {currencyOptions}
+            </select>
+            <div className="input-group">
+              <div className="input-group-prepend">
+                <div className="input-group-text">{currencies[quoteAcronym].symbol}</div>
+              </div>
+              <input id="quote" className="form-control form-control-lg" value={quoteValue} onChange={this.changeQuoteValue} type="number" />
+            </div>
+            <small className="text-secondary">{currencies[quoteAcronym].name}</small>
+          </div>
+        </form>
+      </React.Fragment>
+    )
   }
-  
-  class CurrencyInput extends React.Component {
-    render() {
-      const { value, onChange } = this.props;
-  
-      return <input value={value} onChange={onChange} type="number" />
-    }
-  }
-  
-  ReactDOM.render(
-    <CurrencyConverter />,
-    document.getElementById('root')
-  );
+}
 
 
-  export default CurrencyConverter
+export default CurrencyConverter
+
+
