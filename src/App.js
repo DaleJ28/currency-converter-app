bb28d1ce1decb8a3740d96d17a60c46bd5917795
Dale Jiblal <70293940+DaleJ28@users.noreply.github.com>
2023-10-04 01:52:17 -0400

    fixed router basename path,+404.html

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 
 const App = () => {
   return (
-    <Router basename="/currency-converter">
+    <Router basename="/currency-converter-app">
       <Layout>
         <Switch>
           <Route path="/" exact component={Home} />
